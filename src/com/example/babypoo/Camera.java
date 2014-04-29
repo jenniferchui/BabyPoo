4010a7a9e9cd8633ad96dd95bfe560b7f401c948
jenniferchui <jenniferchui@users.noreply.github.com>
2014-04-29 01:24:38 -0700

    Application: Remove Unused libraries
    Signed-off-by: jenniferchui <jenniferchui@users.noreply.github.com>

--- src/com/example/babypoo/Camera.java
+++ src/com/example/babypoo/Camera.java
@@ -4,15 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.LayoutInflater;
@@ -22,7 +21,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.os.Build;
 import android.provider.MediaStore;
 
 public class Camera extends ActionBarActivity {
