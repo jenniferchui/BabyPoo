e14109af68ed161594b1751f9de050d811aa1a79
jenniferchui <jenniferchui@users.noreply.github.com>
2014-04-29 01:25:15 -0700

    UI: add debug counters for white, brown, black
    Signed-off-by: jenniferchui <jenniferchui@users.noreply.github.com>

--- gen/com/example/babypoo/R.java
+++ gen/com/example/babypoo/R.java
@@ -990,9 +990,12 @@ containing a value of this type.
         public static final int normal3=0x7f02005d;
     }
     public static final class id {
-        public static final int abnormal1=0x7f050047;
-        public static final int abnormal2=0x7f050042;
-        public static final int abnormal3=0x7f050046;
+        public static final int TotalBlack=0x7f050041;
+        public static final int TotalBrown=0x7f050042;
+        public static final int TotalWhite=0x7f050040;
+        public static final int abnormal1=0x7f05004a;
+        public static final int abnormal2=0x7f050045;
+        public static final int abnormal3=0x7f050049;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1004,12 +1007,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050048;
+        public static final int action_settings=0x7f05004b;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003f;
-        public static final int check_chart=0x7f050041;
+        public static final int button1=0x7f05003e;
+        public static final int check_chart=0x7f050044;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1026,17 +1029,17 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f05003e;
+        public static final int imageView1=0x7f05003f;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int match_photo=0x7f050040;
+        public static final int match_photo=0x7f050043;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
-        public static final int noraml2=0x7f050044;
-        public static final int noraml3=0x7f050045;
+        public static final int noraml2=0x7f050047;
+        public static final int noraml3=0x7f050048;
         public static final int normal=0x7f050000;
-        public static final int normal1=0x7f050043;
+        public static final int normal1=0x7f050046;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
